8fbd6ada2a3c8ba350aafc42599a2b9737f2a914
zhw <1914624121@qq.com>
2020-05-04 23:43:05 +0800

    提交人:张伟 提交原因：源码学习

---
+++ springsource/src/main/java/com/example/zhw/springsource/SpringsourceApplication.java
@@ -0,0 +1,15 @@
+package com.example.zhw.springsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsourceApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringsourceApplication.class, args);
+	}
+
+}
---
+++ springsource/src/test/java/com/example/zhw/springsource/SpringsourceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.zhw.springsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsourceApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
