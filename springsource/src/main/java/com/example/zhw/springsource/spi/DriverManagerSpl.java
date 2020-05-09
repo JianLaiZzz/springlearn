32c239d79f616670dbfc9afb8e91cab97961260d
zhw <1914624121@qq.com>
2020-05-09 21:08:56 +0800

    提交人:张伟 提交原因：破坏双亲委派原则的spi my数据库加载驱动类

---
+++ springsource/src/main/java/com/example/zhw/springsource/spi/DriverManagerSpl.java
@@ -0,0 +1,24 @@
+package com.example.zhw.springsource.spi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author: zhw
+ * @createDate: 2020/5/6
+ */
+public class DriverManagerSpl
+{
+	static String url = "jdbc:mysql://120.78.132.139:3306/book?useUnicode=true&characterEncoding=utf8";
+	static String user = "root";
+	static String password = "123456";
+
+	public static void main(String args[]) throws ClassNotFoundException, SQLException
+	{
+
+		 			Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection(url, user, password);
+
+	}
+}
