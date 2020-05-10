26c4c07592d5e82d4c448bc6368f26e6aa9aa45e
zhw <1914624121@qq.com>
2020-05-10 11:02:08 +0800

    提交人：张伟 提交原因：提交pom文件

--- springsource/src/main/java/com/example/zhw/springsource/SpringsourceApplication.java
+++ springsource/src/main/java/com/example/zhw/springsource/SpringsourceApplication.java
@@ -9,7 +9,7 @@ public class SpringsourceApplication
 
 	public static void main(String[] args)
 	{
-		System.out.println("zhagwwerwer１１");
+		System.out.println("zhagwwerwer１１212");
 		SpringApplication.run(SpringsourceApplication.class, args);
 		 System.out.println("hehhe1113213123123");
 	}
