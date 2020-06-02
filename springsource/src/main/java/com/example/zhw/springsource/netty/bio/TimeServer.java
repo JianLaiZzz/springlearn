669beb75371746bc5001deeea9f2bb0156fdecea
zhw <zhangwei1@hua-cloud.com.cn>
2020-06-02 12:00:46 +0800

    提交人：张伟 提交原因：netty学习

---
+++ springsource/src/main/java/com/example/zhw/springsource/netty/bio/TimeServer.java
@@ -0,0 +1,12 @@
+package com.example.zhw.springsource.netty.bio;
+
+/**
+ * @author zhangwei1
+ * @date 2020/6/2 11:52
+ */
+public class TimeServer {
+    public  void bind(int port){
+
+
+    }
+}
