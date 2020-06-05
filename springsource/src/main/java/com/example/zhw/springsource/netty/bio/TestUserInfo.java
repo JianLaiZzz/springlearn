fc9d2bedb6479851f661079e4278665aa96899dd
zhw <zhangwei1@hua-cloud.com.cn>
2020-06-05 14:47:20 +0800

    提交人：张伟 提交原因：jdk 和 netty序列化对比

---
+++ springsource/src/main/java/com/example/zhw/springsource/netty/bio/TestUserInfo.java
@@ -0,0 +1,35 @@
+package com.example.zhw.springsource.netty.bio;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * @author zhangwei1
+ * @date 2020/6/5 11:54
+ */
+public class TestUserInfo {
+    public static void main(String[] args) throws IOException {
+        UserInfo userInfo = new UserInfo();
+
+        userInfo.buildUserId(100).buildUserName("Welcome to Netty");
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        ObjectOutputStream os = new ObjectOutputStream(bos);
+
+        os.writeObject(userInfo);
+        os.flush();
+        os.close();
+
+        byte[] b = bos.toByteArray();
+
+        System.out.println("The jdk Serializable length is :" + b.length);
+        bos.close();
+
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("The byte array Serializable length is :" + userInfo.codeC().length);
+
+    }
+}
