8cdd9b8d8c02d9c4117d40c2eeded887a0a58eda
zhw <1914624121@qq.com>
2020-06-25 14:24:40 +0800

    把以前学习的springboot项目加进来

--- springsource/src/main/java/com/example/zhw/springsource/java8/stream/StreamTest.java
+++ springsource/src/main/java/com/example/zhw/springsource/java8/stream/StreamTest.java
@@ -8,27 +8,26 @@ import java.util.stream.Collectors;
  * @author zhangwei1
  * @date 2020/6/24 15:02
  */
-public class StreamTest {
-    public static void main(String[] args) {
+public class StreamTest
+{
+	public static void main(String[] args)
+	{
 
+		List<Integer> list = Arrays.asList(1, 2, -1, -3, 5, 9);
 
-        List<Integer> list = Arrays.asList(1, 2, -1, -3, 5, 9);
+		long count = list.parallelStream().filter(i -> i > 0).count();
 
-        long count = list.parallelStream().filter(i -> i > 0).count();
+		//System.out.println("Positive count: " + count);
 
-        //System.out.println("Positive count: " + count);
+		//list.stream().forEach(s -> System.out.println(s));
 
-        //list.stream().forEach(s -> System.out.println(s));
+		list.stream().map(s -> Math.abs(s)).forEach(s -> System.out.println(s));
 
-        list.stream().map(s -> Math.abs(s)).forEach(s -> System.out.println(s));
+		List<String> list1 = Arrays.asList("1 2", "3 4", "5 6");
 
-        List<String> list1 = Arrays.asList("1 2", "3 4", "5 6");
+		list1.stream().flatMap(item -> Arrays.stream(item.split(" "))).forEach(System.out::print);
 
-        list1.stream().flatMap(item -> Arrays.stream(item.split(" "))).forEach(System.out::print);
+		list1.stream().flatMap(item -> Arrays.stream(item.split(" "))).collect(Collectors.toList());
 
-
-        list1.stream().flatMap(item -> Arrays.stream(item.split(" "))).collect(Collectors.toList());
-
-
-    }
+	}
 }
