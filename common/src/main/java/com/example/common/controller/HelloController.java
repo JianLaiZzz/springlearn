dced3bc9e2f5ee1059690cbeb0eae2d84d1032d9
zhw <1914624121@qq.com>
2020-09-28 22:41:25 +0800

    logback配置

---
+++ common/src/main/java/com/example/common/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.common.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "hello")
+@Slf4j
+public class HelloController {
+
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
+    @GetMapping("/hello")
+    public void hello() {
+        log.info("刘亦菲");
+    }
+
+
+}
---
+++ common/src/test/java/com/example/common/LoggerTest.java
@@ -0,0 +1,22 @@
+package com.example.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+
+    @Test
+    public void test2(){
+        log.debug("debug啥打法是否");//默认日志级别为info
+        log.info("info阿范德萨");
+        log.error("error 阿范德萨");
+        log.warn("warn 阿范德萨");
+    }
+}
