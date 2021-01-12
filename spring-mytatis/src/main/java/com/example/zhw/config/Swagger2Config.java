4ee7898faa84fc4134b564a474c68802784ad99f
zhw <123@qq.com>
2021-01-12 11:53:23 +0800

    目录约束子模块版本号

--- spring-mytatis/src/main/java/com/example/zhw/config/Swagger2Config.java
+++ spring-mytatis/src/main/java/com/example/zhw/config/Swagger2Config.java
@@ -21,21 +21,18 @@ import java.util.List;
  */
 @Configuration
 @EnableSwagger2
-public class Swagger2Config
-{
-	@Bean
-	public Docket createRestApi()
-	{
-		List<Parameter> pars = new ArrayList<Parameter>();
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-				.paths(PathSelectors.any()).build().globalOperationParameters(pars).apiInfo(apiInfo());
-	}
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi() {
+        List<Parameter> pars = new ArrayList<Parameter>();
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any()).build().globalOperationParameters(pars).apiInfo(apiInfo());
+    }
 
-	private ApiInfo apiInfo()
-	{
-		return new ApiInfoBuilder().title("拾花酿春 RESTful API").description("展示先做基础功能，后面再添加业务")
-				.termsOfServiceUrl("https://www.cnblogs.com/xiebq/").version("1.0").build();
-	}
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("拾花酿春 RESTful API").description("展示先做基础功能，后面再添加业务")
+                .termsOfServiceUrl("https://www.cnblogs.com/xiebq/").version("1.0").build();
+    }
 
 }
